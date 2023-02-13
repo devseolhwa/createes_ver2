2a067a2a62afc68e8aa0a5338a448144fda948a5
Seol hwa <devseolhwa@gmail.com>
2023-02-13 11:33:22 +0900

    test

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
 
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
     let ww = $(window).width();
     let html = $("html");
     let loading = $(".loading");
